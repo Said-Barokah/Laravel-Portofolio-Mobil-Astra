2b5bdfa958747854bc81b6e28695a7de724938d8
Said-Barokah <yanginiemailnyasaid@gmail.com>
2024-10-01 12:29:13 +0700

    Menambahkan proyek untuk menunjukkan aktivitas di GitHub

--- resources/views/Admin.blade.php
+++ resources/views/Admin.blade.php
@@ -9,7 +9,7 @@
             </div>
             <div>
                 <a href="{{route('admin.create')}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Tambahkan Data
+                    Tambah Data
                 </a>
             </div>
         </div>
